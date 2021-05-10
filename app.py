e63edeafefb3c21f04cc4a7c98905f5ac863c550
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-10 00:33:53 -0400

    app.py

--- app.py
+++ app.py
@@ -133,3 +133,78 @@ def start_only(start):
 
     # Check for valid entry of start date
     valid_entry = session.query(exists().where(Measurement.date == start)).scalar()
+
+  if valid_entry:
+
+    	results = (session.query(func.min(Measurement.tobs)
+    				 ,func.avg(Measurement.tobs)
+    				 ,func.max(Measurement.tobs))
+    				 	  .filter(Measurement.date >= start).all())
+
+    	tmin =results[0][0]
+    	tavg ='{0:.4}'.format(results[0][1])
+    	tmax =results[0][2]
+    
+    	result_printout =( ['Entered Start Date: ' + start,
+    						'The lowest Temperature was: '  + str(tmin) + ' F',
+    						'The average Temperature was: ' + str(tavg) + ' F',
+    						'The highest Temperature was: ' + str(tmax) + ' F'])
+    	return jsonify(result_printout)
+
+    return jsonify({"error": f"Input Date {start} not valid. Date Range is {date_range_min_str} to {date_range_max_str}"}), 404
+   
+
+@app.route("/api/v1.0/<start>/<end>") # Calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive
+def start_end(start, end):
+
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    # Date Range (only for help to user in case date gets entered wrong)
+    date_range_max = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    date_range_max_str = str(date_range_max)
+    date_range_max_str = re.sub("'|,", "",date_range_max_str)
+    print (date_range_max_str)
+
+    date_range_min = session.query(Measurement.date).first()
+    date_range_min_str = str(date_range_min)
+    date_range_min_str = re.sub("'|,", "",date_range_min_str)
+    print (date_range_min_str)
+
+    # Check for valid entry of start date
+    valid_entry_start = session.query(exists().where(Measurement.date == start)).scalar()
+ 	
+ 	# Check for valid entry of end date
+    valid_entry_end = session.query(exists().where(Measurement.date == end)).scalar()
+
+    if valid_entry_start and valid_entry_end:
+
+    	results = (session.query(func.min(Measurement.tobs)
+    				 ,func.avg(Measurement.tobs)
+    				 ,func.max(Measurement.tobs))
+    					  .filter(Measurement.date >= start)
+    				  	  .filter(Measurement.date <= end).all())
+
+    	tmin =results[0][0]
+    	tavg ='{0:.4}'.format(results[0][1])
+    	tmax =results[0][2]
+    
+    	result_printout =( ['Entered Start Date: ' + start,
+    						'Entered End Date: ' + end,
+    						'The lowest Temperature was: '  + str(tmin) + ' F',
+    						'The average Temperature was: ' + str(tavg) + ' F',
+    						'The highest Temperature was: ' + str(tmax) + ' F'])
+    	return jsonify(result_printout)
+
+    if not valid_entry_start and not valid_entry_end:
+    	return jsonify({"error": f"Input Start {start} and End Date {end} not valid. Date Range is {date_range_min_str} to {date_range_max_str}"}), 404
+
+    if not valid_entry_start:
+    	return jsonify({"error": f"Input Start Date {start} not valid. Date Range is {date_range_min_str} to {date_range_max_str}"}), 404
+
+    if not valid_entry_end:
+    	return jsonify({"error": f"Input End Date {end} not valid. Date Range is {date_range_min_str} to {date_range_max_str}"}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
