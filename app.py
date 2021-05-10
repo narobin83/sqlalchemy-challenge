ce65b8916d568a33e527adfb26d8c856c8973a09
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-10 00:04:36 -0400

    app.py

--- app.py
+++ app.py
@@ -27,3 +27,33 @@ Station = Base.classes.station
 
 app = Flask(__name__)
 
+ """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start (enter as YYYY-MM-DD)<br/>"
+        f"/api/v1.0/start/end (enter as YYYY-MM-DD/YYYY-MM-DD)"
+
+    )
+
+ @app.route("/api/v1.0/precipitation") 
+def precipitation():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query Measurement
+    results = (session.query(Measurement.date, Measurement.tobs)
+                      .order_by(Measurement.date))
+
+   # Create a dictionary
+    precipitation_date_tobs = []
+    for each_row in results:
+        dt_dict = {}
+        dt_dict["date"] = each_row.date
+        dt_dict["tobs"] = each_row.tobs
+        precipitation_date_tobs.append(dt_dict)
+
+    return jsonify(precipitation_date_tobs)
+
